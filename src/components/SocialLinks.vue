bb04a1d8a9cdd551854b0b8a61d606c0a5b3d780
nahid ferdous <nahidferdous240@gmail.com>
2025-04-13 20:56:19 +0600

    !

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,22 +1,25 @@
 <template>
-  <div v-if="siteData.socials.length" class="flex gap-6">
-    <template
-        v-for="social in siteData.socials"
-        :key="social.name"
-    >
-      <a
-          v-if="social.link"
-          :href="social.link"
-          target="_blank"
-          data-hover
+  <div>
+    <p class="mb-2 font-medium">FOLLOW ME</p>
+    <div v-if="siteData.socials.length" class="flex gap-6">
+      <template
+          v-for="social in siteData.socials"
+          :key="social.name"
       >
-        <Icon
-            v-if="social.icon"
-            :name="social.icon"
-            class="w-6 h-6 text-gray-800 dark:text-gray-200 hover:text-primary dark:hover:text-primary"
-        />
-      </a>
-    </template>
+        <a
+            v-if="social.link"
+            :href="social.link"
+            target="_blank"
+            data-hover
+        >
+          <Icon
+              v-if="social.icon"
+              :name="social.icon"
+              class="w-6 h-6 text-gray-800 dark:text-gray-200 hover:text-primary dark:hover:text-primary"
+          />
+        </a>
+      </template>
+    </div>
   </div>
 </template>
 
