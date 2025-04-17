6e45ca2e340721da6774e8d19f865d641418e806
nahid ferdous <nahidferdous240@gmail.com>
2025-04-17 22:58:27 +0600

    !

--- src/components/Home/Skills.vue
+++ src/components/Home/Skills.vue
@@ -2,33 +2,35 @@
   <section class="contain content-section">
     <UiHeading>Skills</UiHeading>
 
-    <template v-for="(skillCategory, index) in siteData.skills" :key="index">
-      <div class="grid gap-4 mx-4 my-10 sm:grid-cols-12">
-        <div class="col-span-12 sm:col-span-3">
-          <UiSubheading>{{ index }}</UiSubheading>
-        </div>
-        <div class="relative col-span-12 px-4 space-y-6 sm:col-span-9">
-          <div class="flex flex-wrap gap-10 md:gap-x-16 mb-10 max-w-3xl">
-            <div
-                v-for="(skill, skillIndex) in skillCategory"
-                :key="skillIndex"
-            >
-              <UiAnimate
-                  :distance="60"
-                  :delay="0.2 + skillIndex * 0.1"
+    <div class="mt-10">
+      <template v-for="(skillCategory, index) in siteData.skills" :key="index">
+        <div class="grid gap-4 mx-4 my-4 sm:grid-cols-12">
+          <div class="col-span-12 sm:col-span-3">
+            <UiSubheading>{{ index }}</UiSubheading>
+          </div>
+          <div class="relative col-span-12 px-4 space-y-6 sm:col-span-9">
+            <div class="flex flex-wrap gap-10 md:gap-x-16 mb-10 max-w-3xl">
+              <div
+                  v-for="(skill, skillIndex) in skillCategory"
+                  :key="skillIndex"
               >
-                <div class="relative flex flex-row items-center gap-4">
+                <UiAnimate
+                    :distance="60"
+                    :delay="0.2 + skillIndex * 0.1"
+                >
+                  <div class="relative flex flex-row items-center gap-4">
                 <span>
                   <NuxtImg :src="skill.logo" :alt="skill.name" class="h-9" loading="lazy"/>
                 </span>
-                  <span>{{ skill.name }}</span>
-                </div>
-              </UiAnimate>
+                    <span>{{ skill.name }}</span>
+                  </div>
+                </UiAnimate>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </template>
+      </template>
+    </div>
   </section>
 </template>
 
