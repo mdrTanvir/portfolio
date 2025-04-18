b764e5f60a6abe31a0ab47e2c21324f774981474
nahid ferdous <nahidferdous240@gmail.com>
2025-04-18 15:26:46 +0600

    !

--- src/components/Hero/HeroStats.vue
+++ src/components/Hero/HeroStats.vue
@@ -2,7 +2,9 @@
   <div class="flex flex-wrap gap-3 sm:gap-4 mt-10">
     <template v-for="(stat, n) in siteData.stats">
       <UiAnimate :distance="10" :delay="1.2 + n * 0.2" :duration="0.6" scrollTriggerStart="center bottom" :scrub="false">
-        <UiDotCard class="w-auto md:w-[170px] py-3 md:py-5" :title="stat.title" :subtitle="stat.subtitle"/>
+        <UiDotCard class="w-auto md:w-[170px] py-3 md:py-5" :title="stat.title" :subtitle="stat.subtitle"
+                   :responsiveText="true"
+        />
       </UiAnimate>
     </template>
   </div>
--- src/components/Ui/DotCard.vue
+++ src/components/Ui/DotCard.vue
@@ -2,17 +2,29 @@
   <div class="relative sm:ml-4 px-5 md:px-8 py-3 flex flex-col bg-gray-100 dark:bg-gray-900
         border-2 border-indigo-400 rounded-xl md:rounded-2xl shadow-md max-w-[400px]">
     <!-- Dot -->
-<!--    <div class="timeline-dot"></div>-->
+    <!--    <div class="timeline-dot"></div>-->
     <div class="block absolute w-6 h-6 md:w-10 md:h-10 rounded-full left-0 top-1/2 -translate-y-1/2 -translate-x-1/2 z-[1]
                 bg-primary dark:bg-primary border-[7px] md:border-[10px] border-white dark:border-black"></div>
     <slot>
-      <h3 v-if="title" class="text-lg font-semibold text-primary">
+      <h3
+          v-if="title"
+          class="sm:text-lg font-semibold text-primary"
+          :class="{'text-sm': responsiveText}"
+      >
         {{ title }}
       </h3>
-      <p v-if="subtitle" class="text-sm text-gray-500 dark:text-gray-300 mix-blend-difference">
+      <p
+          v-if="subtitle"
+          class="sm:text-sm text-gray-500 dark:text-gray-300 mix-blend-difference"
+          :class="{'text-xs': responsiveText}"
+      >
         {{ subtitle }}
       </p>
-      <p v-if="description" class="text-xs text-gray-400 mt-1 mix-blend-difference">
+      <p
+          v-if="description"
+          class="sm:text-xs text-gray-400 mt-1 mix-blend-difference"
+          :class="{'text-xs': responsiveText}"
+      >
         {{ description }}
       </p>
     </slot>
@@ -24,5 +36,6 @@ defineProps({
   title: {type: String, default: '',},
   subtitle: {type: String, default: '',},
   description: {type: String, default: '',},
+  responsiveText: {type: Boolean, default: false,},
 });
 </script>
