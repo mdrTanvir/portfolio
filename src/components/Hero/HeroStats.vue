b448cf040a114670d03a72f46725b7dea8418f64
nahid ferdous <nahidferdous240@gmail.com>
2025-04-18 15:32:52 +0600

    !

--- src/components/Hero/HeroStats.vue
+++ src/components/Hero/HeroStats.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex flex-wrap gap-3 sm:gap-4 mt-10">
+  <div class="flex flex-wrap gap-4 mt-10">
     <template v-for="(stat, n) in siteData.stats">
       <UiAnimate :distance="10" :delay="1.2 + n * 0.2" :duration="0.6" scrollTriggerStart="center bottom" :scrub="false">
-        <UiDotCard class="w-auto md:w-[170px] py-3 md:py-5" :title="stat.title" :subtitle="stat.subtitle"
+        <UiDotCard class="w-auto sm:w-[170px] py-3 md:py-5" :title="stat.title" :subtitle="stat.subtitle"
                    :responsiveText="true"
         />
       </UiAnimate>
