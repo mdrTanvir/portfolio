1086860e6627f93c16287c007a05e0ee88c44342
nahid ferdous <nahidferdous240@gmail.com>
2025-04-22 16:45:59 +0600

    !

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import isProduction from "./src/config/env";
+// import isProduction from "./src/config/env";
 
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
-    // ssr: true,
-    ssr: isProduction(),
+    ssr: true,
+    // ssr: isProduction(),
     srcDir: './src/',
     typescript: {
         strict: true,
