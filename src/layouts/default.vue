4bfa5df27ff34a1e2822fa7b5d7ce51a3e4df2d6
nahid ferdous <nahidferdous240@gmail.com>
2025-04-25 00:22:00 +0600

    cursor improvements

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="relative overflow-hidden bg-white dark:bg-black text-black dark:text-white">
-    <template v-if="isDesktop">
-      <LazyUiGradient/>
-    </template>
+    <LazyUiGradient/>
     <UiParticles :key="count"/>
     <NavBar/>
     <NuxtPage/>
@@ -12,11 +10,10 @@
 </template>
 
 <script setup lang="ts">
-const {isDesktop} = useIsDesktop()
 const themeStore = useThemeStore()
 const {darkMode} = storeToRefs(themeStore)
-const count = ref(0)
 
+const count = ref(0)
 const increment = () => count.value++
 
 watch(() => darkMode.value, () => increment())
