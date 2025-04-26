ed1b0d417545716b04d95673f5779f02cc8fd633
nahid ferdous <nahidferdous240@gmail.com>
2025-04-26 11:05:48 +0600

    cursor improvements

--- src/components/Home/HeroAbout.vue
+++ src/components/Home/HeroAbout.vue
@@ -2,7 +2,7 @@
   <!-- Hero Section -->
   <section id="HERO" class="heroSection pb-10 md:pb-14 lg:pb-20">
     <div class="contain relative h-[100lvh] min-h-[100lvh] z-10 max-h-[1300px] pt-10 sm:pt-16 md:pt-24 pb-6 md:pb-10">
-      <div class="h-full flex flex-col justify-between relative z-[2]">
+      <div class="h-full flex flex-col justify-center md:justify-between gap-y-20 md:gap-y-2 relative z-[2]">
         <HomeHeroTitle/>
         <HomeHeroStats/>
       </div>
